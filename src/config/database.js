40682e28e63bcfe4543ba219ab1002b6629de8da
Fernando Sandoval <sandovalf193@gmail.com>
2020-07-17 19:03:58 -0600

    heroku deploy

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('interviewTest', 'root', '',{
-    host: 'localhost',
+// const sequelize = new Sequelize('interviewTest', 'root', '',{
+//     host: 'localhost',
+//     dialect: 'mysql'
+// })
+
+const sequelize = new Sequelize('heroku_3816315a6003c09', 'bfce332a648e8e', '99b2ea7c',{
+    host: 'us-cdbr-east-02.cleardb.com',
     dialect: 'mysql'
 })
+
+//:@/?reconnect=true
 
 module.exports = sequelize;
